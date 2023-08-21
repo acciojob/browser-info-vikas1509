6395e4d1368df1a376098b455419b6f400eb2308
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-21 14:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function displayBrowserInfo() {
+            let newDiv = document.creatElement('div');
+newDiv.id='browserInfoDiv';
+            // Retrieve browser information
+            var browserName = navigator.appName;
+            var browserVersion = navigator.appVersion;
+
+            // Update the div with browser information
+            newDiv.textContent = "You are using " + browserName + " version " + browserVersion;
+	document.append(newDiv);
+        }
